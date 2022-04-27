3cf02b1dea51b5d1b71d6fb806cd014e815b2e92
Matteo Penna <penna.matteo@gmail.com>
2022-04-27 08:41:12 +0200

    Added resolver to fetch servers

--- routing-start/src/app/servers/server/server.component.ts
+++ routing-start/src/app/servers/server/server.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params, Router } from '@angular/router';
+import { ActivatedRoute, Data, Params, Router } from '@angular/router';
 
 import { ServersService } from '../servers.service';
 
@@ -16,11 +16,16 @@ export class ServerComponent implements OnInit {
     private router: Router) { }
 
   ngOnInit() {
-    const serverId: number = +this.route.snapshot.params['id'];
-    this.server = this.serversService.getServer(serverId);
-    this.route.params.subscribe((params: Params) => {
-      this.server = this.serversService.getServer(+params['id']);
-    })
+    this.route.data.subscribe(
+      (data: Data) => {
+        this.server = data['server'];
+      }
+    );
+    // const serverId: number = +this.route.snapshot.params['id'];
+    // this.server = this.serversService.getServer(serverId);
+    // this.route.params.subscribe((params: Params) => {
+    //   this.server = this.serversService.getServer(+params['id']);
+    // })
   }
 
   onEditServer() {
