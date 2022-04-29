54c69ee48dad714962233bcda14e7c1f85e5856f
Matteo Penna <penna.matteo@gmail.com>
2022-04-29 22:30:44 +0200

    Add form project

---
+++ forms-td-start/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  @ViewChild('f') signupForm: NgForm;
+
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+  }
+
+  // onSubmit(form: NgForm) {
+  //   console.log(form);
+  // }
+
+  onSubmit() {
+    console.log(this.signupForm);
+  }
+}
