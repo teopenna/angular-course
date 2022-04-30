87064ba19b2d0df8791be2cb0de9f8cd9108fe44
Matteo Penna <penna.matteo@gmail.com>
2022-04-30 22:35:18 +0200

    Handled suggest user name function with setValue and patchValue

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -15,6 +15,21 @@ export class AppComponent {
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: '',
+    //   questionAnswer: 'blah, blah',
+    //   gender: 'male'
+    // });
+    this.signupForm.form.patchValue({
+        userData: {
+          username: suggestedName
+        }
+      }
+    );
   }
 
   // onSubmit(form: NgForm) {
